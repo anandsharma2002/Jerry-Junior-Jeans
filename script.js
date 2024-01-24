c664cd76e2b1b4b7c8e117812612166d69a85e43
Anand Sharma <anandsharma.ts3000@gmail.com>
2024-01-24 21:50:07 +0530

    updated nav bar

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
+const body = document.querySelector("body")
+const menu = document.querySelector('.menu')
+const menulis = document.querySelectorAll('.menuli')
+const menulinks = document.querySelectorAll('.menulink')
+
+
+body.style.backgroundColor = "var(--darkGray1)"
+menu.style.display = "none"
+menulis.forEach(menuli => {
+    menuli.style.backgroundColor = "var(--darkGray2)"
+})
+menulinks.forEach(menulink => {
+    menulink.style.color = "var(--white)"
+})
+
+
+
+
+
+//      ALL FUNCTIONS 
 
 function menubutton() {
+    if(menu.style.display == "none"){
+        menu.style.display = "block"
+    }
+    else{
+        menu.style.display = "none"
+    }
+}
 
+function darkmode() {
+    if(body.style.backgroundColor == "var(--darkGray1)"){
+        body.style.backgroundColor = "var(--white)"
+        menu.style.backgroundColor = "var(--lightGray1)"
+        menu.style.color = "var(--black)"
+        menulis.forEach(menuli => {
+            menuli.style.backgroundColor = "var(--lightGray2)"
+        })
+        menulinks.forEach(menulink => {
+            menulink.style.color = "var(--black)"
+        })
+    }
+    else{
+        body.style.backgroundColor = "var(--darkGray1)"
+        menu.style.backgroundColor = "var(--darkGray2)"
+        menu.style.color = "var(--white)"
+        menulis.forEach(menuli => {
+            menuli.style.backgroundColor = "var(--darkGray2)"
+        })
+        menulinks.forEach(menulink => {
+            menulink.style.color = "var(--white)"
+        })
+    }
 }
