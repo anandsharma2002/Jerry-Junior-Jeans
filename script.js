f1a437bb8bdcf79fa66dfded17d4816e23c0344f
Anand Sharma <anandsharma.ts3000@gmail.com>
2024-01-25 23:51:04 +0530

    updated

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ const story_circleImgs = document.querySelectorAll(".story_circle_img")
 const typeNameboys = document.querySelector('.typeNameBoys')
 const typeNamegirls = document.querySelector('.typeNameGirls')
 const typeNameBoys_animation = document.getElementById('typeNameBoys_animation')
+const typeNameGirls_animation = document.getElementById('typeNameGirls_animation')
 const typeNameButtons = document.querySelectorAll('.typeNameButton')
+const typeNameCards = document.querySelectorAll('.typeNameCard')
+const size_as = document.querySelectorAll('.size_a')
+const footers = document.querySelectorAll('footer')
+const footerli_as = document.querySelectorAll('.footerli_a')
 
 
 
@@ -36,7 +41,22 @@ typeNameButtons.forEach(typeNameButton => {
     typeNameButton.style.color = "var(--white)"
     typeNameButton.style.borderBottom = "2px solid var(--white)"
 })
-
+typeNameBoys_animation.style.backgroundColor = "var(--darkGray1)"
+typeNameGirls_animation.style.backgroundColor = "var(--darkGray1)"
+typeNameCards.forEach(typeNameCard => {
+    typeNameCard.style.backgroundColor = "var(--darkGray2)"
+    typeNameCard.style.color = "var(--white)"
+})
+size_as.forEach(size_a => {
+    size_a.style.color = "var(--white)"
+})
+footers.forEach(footer => {
+    footer.style.backgroundColor = "var(--darkGray3)"
+    footer.style.color = "var(--white)"
+})
+footerli_as.forEach(footerli_a => {
+    footerli_a.style.color = "var(--white)"
+})
 
 
 
@@ -70,14 +90,32 @@ function darkmode() {
             story_circle_img.style.border = "2px solid var(--black)"
         })
         story_section.style.borderBottom = "2px solid var(--black)"
-        typeNameButtons.forEach(typeNameButton => {
-            button.style.color = "var(--black)"
-        })
+        
         typeNameButtons.forEach(typeNameButton => {
             typeNameButton.style.color = "var(--black)"
             typeNameButton.style.borderBottom = "2px solid var(--black)"
         })
-
+        typeNameBoys_animation.style.backgroundColor = "var(--lightGray1)"
+        typeNameGirls_animation.style.backgroundColor = "var(--lightGray1)"
+        typeNameCards.forEach(typeNameCard => {
+            typeNameCard.style.color = "var(--white)"
+            typeNameCard.style.borderBottom = "2px solid var(--white)"
+        })
+        typeNameCards.forEach(typeNameCard => {
+            typeNameCard.style.backgroundColor = "var(--white)"
+            typeNameCard.style.border = "1px solid var(--black)"
+            typeNameCard.style.color = "var(--black)"
+        })
+        size_as.forEach(size_a => {
+            size_a.style.color = "var(--black)"
+        })
+        footers.forEach(footer => {
+            footer.style.backgroundColor = "var(--lightGray3)"
+            footer.style.color = "var(--black)"
+        })
+        footerli_as.forEach(footerli_a => {
+            footerli_a.style.color = "var(--black)"
+        })
     }
     else{
         body.style.backgroundColor = "var(--darkGray1)"
@@ -97,13 +135,25 @@ function darkmode() {
         })
         story_section.style.borderBottom = "2px solid var(--white)"
         typeNameButtons.forEach(typeNameButton => {
-            button.style.color = "var(--white)"
-        })
-        typeNameButtons.forEach(typeNameButton => {
             typeNameButton.style.color = "var(--white)"
             typeNameButton.style.borderBottom = "2px solid var(--white)"
         })
-
+        typeNameBoys_animation.style.backgroundColor = "var(--darkGray1)"
+        typeNameGirls_animation.style.backgroundColor = "var(--darkGray1)"
+        typeNameCards.forEach(typeNameCard => {
+            typeNameCard.style.backgroundColor = "var(--darkGray2)"
+            typeNameCard.style.color = "var(--white)"
+        })
+        size_as.forEach(size_a => {
+            size_a.style.color = "var(--white)"
+        })
+        footers.forEach(footer => {
+            footer.style.backgroundColor = "var(--darkGray3)"
+            footer.style.color = "var(--white)"
+        })
+        footerli_as.forEach(footerli_a => {
+            footerli_a.style.color = "var(--white)"
+        })
     }
 }
 
